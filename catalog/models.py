7e7fb70e8f7bd939b9f77fbf4ceeacdae77ee2eb
Sunil Peesapati <sunilpeesapati@gmail.com>
2020-11-29 14:31:04 +0530

    admin page layouts for books and authors

--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import uuid # Required for unique book instances
 
 class Genre(models.Model):
     """Model representing a book genre."""
@@ -33,8 +34,13 @@ class Book(models.Model):
     def get_absolute_url(self):
         """Returns the url to access a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+        """Create a string for the Genre. This is required to display genre in Admin."""
+        return ', '.join(genre.name for genre in self.genre.all()[:3])
+    
+    display_genre.short_description = 'Genre'
 
-import uuid # Required for unique book instances
 
 class BookInstance(models.Model):
     """Model representing a specific copy of a book (i.e. that can be borrowed from the library)."""
@@ -64,6 +70,7 @@ class BookInstance(models.Model):
     def __str__(self):
         """String for representing the Model object."""
         return f'{self.id} ({self.book.title})'
+
 
 class Author(models.Model):
     """Model representing an author."""
